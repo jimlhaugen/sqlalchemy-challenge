11a4a2a30f10faa11e3197ab9650b0560cc08d3a
James Haugen <jaugen@mindspring.com>
2024-04-15 22:39:47 -0700

    engine creation difficulties; was able to get a portion of app.py working

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 # Import the dependencies.
-# Copy and paste from climate_starter.ipynb file
 
-from matplotlib import style
-style.use('fivethirtyeight')
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -11,25 +8,22 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from flask import Flask, jsonify
+import os
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
 
-
 # reflect the tables
 Base.prepare(autoload_with = engine, reflect = True)
-
 
 # Save references to each table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
-
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -37,8 +31,8 @@ session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
-# 1. import Flask
-from flask import Flask
+# 1. Import Flask
+from flask import Flask, jsonify
 
 # 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
@@ -49,29 +43,32 @@ app = Flask(__name__)
 
 @app.route("/api/v1.0/precipitation")
 def Precipitation():
-    """Convert the query results from your precipitation analysis (i.e., retrieve only the last 12 months of data) to a dictionary using <date> as the key and <prcp> as the value."""
-    """Returns the JSON represenation of the dictionary"""
-    dictionary = session.query(Measurement.date, Measurement.prcp).\
-    filter(Measurement.date >= '2016-08-23')
-
-    return jsonify(dictionary)
+     """Convert the query results from your precipitation analysis (i.e., retrieve only the last 12 months of data) to a dictionary using <date> as the key and <prcp> as the value."""
+     """Returns the JSON represenation of the dictionary"""
+     dictionary = session.query(Measurement.date, Measurement.prcp).\
+     filter(Measurement.date >= '2016-08-23')
+     dictionary=[each_result[0] for each_result in dictionary]
+     return jsonify(dictionary)
 
 
 @app.route("/api/v1.0/stations")
 def Stations():
     """Return a JSON list of stations the dataset."""
 
-    active_stations = session.query(Measurement.station,func.count(Measurement.station)).\
-    group_by(Measurement.station).\
-    order_by(func.count(Measurement.station).desc()).all()
+    active_stations=session.query(Measurement.station).limit(5).all()
+    print(active_stations)
+    active_stations=[each_result[0] for each_result in active_stations]
     return jsonify(active_stations)
+
 
 @app.route("/api/v1.0/tobs")
 def Tobs():
     """Returns a JSON list of temperature observations of the previous year"""
     tobs = session.query(Measurement.tobs).\
     filter(Measurement.date.between('2016-08-23', '2017-08-23')).all()  # between is inclusive
-    return jsonify(tobs)
+    results=[each_result[0] for each_result in tobs]
+    return jsonify(results)
+
 
 @app.route("/api/v1.0/<start>")
 def start_date():
@@ -87,6 +84,7 @@ def start_date():
     min_avg_max_return = {'TMIN':min_avg_max_list[0], 'TAVG':min_avg_max_list[1], 'TMAX':min_avg_max_list[2]}
     return jsonify(min_avg_max_return)
 
+
 @app.route("/api/v1.0/<start>/<end>")
 def start_to_end():
     """Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature from the start date to the end date, inclusive"""
@@ -100,3 +98,6 @@ def start_to_end():
     min_avg_max_list = list(np.ravel(min_avg_max))
     min_avg_max_return = {'TMIN':min_avg_max_list[0], 'TAVG':min_avg_max_list[1], 'TMAX':min_avg_max_list[2]}
     return jsonify(min_avg_max_return)
+
+if __name__ == "__main__":
+    app.run(debug=True)
